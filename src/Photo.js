724774c62c9d02de08a0cce47bc21836a484c870
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2021-04-11 20:06:05 -0500

    Resolving issues part 1

--- src/Photo.js
+++ src/Photo.js
@@ -1,40 +1,64 @@
-import React from 'react' 
-import styled, { keyframes } from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import './Photo.css';
 
-const KF = keyframes`
-    25% {
-        transform: scale(0.8)
-    }
-    50% {
-        transform: scale(1.2)
-    }
-    100% {
-        transform (1.0)
-    }
-`
-
-const StyledImg = styled.img`
-    border: 10px solid ${pr => pr.theme.borderColor};
-    &:hover {
-        animation: ${KF} 1s forward;
-    }
-`
-
-const StyledDetails = styled.div`
+const ContainerDiv = styled.div`
     display: flex;
-    justify-content: space-around;
+    flex-direction: column;
 `
 
-export default function Photo(props) {
-    const { imgURL, copyRight, date } = props
+const picDiv = styled.div`
+    padding: 0.75rem;
+    width: 80%;
+    min-height: 500px;
+    display: flex;
+    flex-direction: column;
+`
+
+const PicBtn = styled.button`
+    border: ${props => props.wheatBorder};
+    padding: 1.5rem;
+    color: ${props => props.primaryColor};
+    background-color: ${props => props.secondaryColor};
+    font-weight: bolder;
+    font-size: 16px;
+    margin: 1.5rem;
+
+    &:hover{
+        border: ${props => props.blackBorder};
+        background-color: ${props => props.primaryColor};
+        color: ${props => props.secondaryColor};
+    }
+`
+
+const PicImg = styled.img`
+    border: ${prop => props.wheatBorder};
+    box-shadow: 8px 8px 8px 8px #000000;
+`
+
+const Pic = () => {
+    const [picData, setPicData] = useState('');
+    const [count, setCount] = useState(0);
+
+    useEffect((event) => {
+        const url = `https://api.nasa.gov/planetary/apod?api_key=V0WSaey6S6VCtwwhHuUVzDY7giVQ2BTTmcxz3EO6&date=2021-04-11${count}`
+
+    axios
+        .get(url)
+        .then(response => setPicData(response.data.url))
+        .catch(error => console.log(error))
+        setPic()
+    }, [count], () => count);
 
     return (
-        <div className = 'main-img'>
-            <StyledImg src = {imgURL} alt = "NASA's Daily Photo" />
-            <StyledDetails className = 'img-details'>
-                <h6>Copyright: {copyRight}</h6>
-                <h6>Date: {date}</h6>
-            </StyledDetails>
-        </div>
+        <ContainerDiv>
+            <picDiv>
+                <PicImg src={picData} alt='Nasa Pic of the Day'  />
+                <PicBtn onClick = {() => setCount(count === 10 ? count - 10 : count + 1)}>Click for New Image</PicBtn>
+            </picDiv>
+        </ContainerDiv>
     )
 }
+
+export default Pic;
